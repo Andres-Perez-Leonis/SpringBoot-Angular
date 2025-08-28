69e8cfcacd7e548f6bdb4bf5bdbbe00b0dbf90d9
Andres Perez Leonis <apleonis@gmail.com>
2025-08-28 14:17:17 +0200

    Project Test File Created

---
+++ planning_SpringBoot/src/test/java/com/andres/planning/repository/ProjectTest.java
@@ -0,0 +1,10 @@
+package com.andres.planning.repository;
+
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.jdbc.Sql;
+
+@SpringBootTest
+@Sql(scripts = "/clean-data.sql")
+public class ProjectTest {
+    
+}
