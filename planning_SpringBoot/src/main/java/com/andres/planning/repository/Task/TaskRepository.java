0cf3a9038932a1db79714ab763d6476524158bef
Andres Perez Leonis <apleonis@gmail.com>
2025-08-29 16:02:58 +0200

    Modified Queries to COrrect Names

--- planning_SpringBoot/src/main/java/com/andres/planning/repository/Task/TaskRepository.java
+++ planning_SpringBoot/src/main/java/com/andres/planning/repository/Task/TaskRepository.java
@@ -5,15 +5,16 @@ import java.util.List;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import com.andres.planning.model.ProjectEntity;
 import com.andres.planning.model.TaskEntity;
 
 public interface TaskRepository extends JpaRepository<TaskEntity, Long>{
     public TaskEntity findByTitle(String title);
-    public List<TaskEntity> findByStatus(String status);
+    public List<TaskEntity> findByCompleted(Boolean completed);
     public List<TaskEntity> findByCategory(String category);
-    public List<TaskEntity> findByCategoryAndStatus(String category, Boolean status);
-    public List<TaskEntity> findByProjectAndCategoryAndStatus(Long projectId, String category, Boolean status);
-    public List<TaskEntity> findByCategoryAndStatusAndDateAfterAndDateBefore(String category, Boolean status, LocalDateTime dateAfter, LocalDateTime dateBefore);
-    public List<TaskEntity> findByProjectAndCategoryAndStatusAndDateAfterAndDateBefore(Long projectId, String category, Boolean status, LocalDateTime dateAfter, LocalDateTime dateBefore);
-    public List<TaskEntity> findByProjectId(Long projectId);
+    public List<TaskEntity> findByCategoryAndCompleted(String category, Boolean completed);
+    public List<TaskEntity> findByProjectAndCategoryAndCompleted(ProjectEntity projectEntity, String category, Boolean completed);
+    public List<TaskEntity> findByCategoryAndCompletedAndStartTimeAndFinishTime(String category, Boolean completed, LocalDateTime startTime, LocalDateTime finishTime);
+    public List<TaskEntity> findByProjectAndCategoryAndCompletedAndStartTimeAndFinishTime(ProjectEntity projectEntity, String category, Boolean completed, LocalDateTime startTime, LocalDateTime finishTime);
+    public List<TaskEntity> findByProject(ProjectEntity projectEntity);
 }
