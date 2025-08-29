5a106e734c8d69be7caa9ae4be16f83776152ff8
Andres Perez Leonis <apleonis@gmail.com>
2025-08-29 15:44:58 +0200

    Modified to references the Task

--- planning_SpringBoot/src/main/java/com/andres/planning/model/SubTaskEntity.java
+++ planning_SpringBoot/src/main/java/com/andres/planning/model/SubTaskEntity.java
@@ -1,19 +1,12 @@
 package com.andres.planning.model;
 
-import java.io.Serializable;
 import java.time.LocalDateTime;
 
 import io.micrometer.common.lang.NonNull;
-import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
 import jakarta.persistence.JoinColumn;
 import jakarta.persistence.ManyToOne;
 import jakarta.persistence.Table;
-import jakarta.persistence.Temporal;
-import jakarta.persistence.TemporalType;
 
 @Entity
 @Table(name = "sub_tasks")
@@ -23,7 +16,7 @@ public class SubTaskEntity extends TaskEntity{
     @ManyToOne
     @JoinColumn(name = "task_id")
     @NonNull
-    private Long taskID;
+    private TaskEntity task;
 
     public SubTaskEntity() {}
 
@@ -35,12 +28,12 @@ public class SubTaskEntity extends TaskEntity{
     // Getters and Setters
 
 
-    public Long getTaskID() {
-        return taskID;
+    public TaskEntity getTask() {
+        return task;
     }
 
-    public void setTaskID(Long taskID) {
-        this.taskID = taskID;
+    public void setTask(TaskEntity task) {
+        this.task = task;
     }
 
 }
