0de2731c21d3f8fccc0c6fd542b4e1ca5fe13d6a
Andres Perez Leonis <apleonis@gmail.com>
2025-08-29 12:13:15 +0200

    Create SubTask - Test Added

--- planning_SpringBoot/src/test/java/com/andres/planning/repository/SubTaskTest.java
+++ planning_SpringBoot/src/test/java/com/andres/planning/repository/SubTaskTest.java
@@ -1,10 +1,30 @@
 package com.andres.planning.repository;
 
+import static org.assertj.core.api.Assertions.assertThat;
+
+import java.time.LocalDateTime;
+
+import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.jdbc.Sql;
+
+import com.andres.planning.model.SubTaskEntity;
+import com.andres.planning.repository.SubTask.SubTaskRepository;
 
 @SpringBootTest
 @Sql(scripts = "/clean-data.sql")
 public class SubTaskTest {
-    
+    private SubTaskRepository subTaskRepository;
+
+    @Test
+    public void createSubTask() {
+        SubTaskEntity subTask = new SubTaskEntity("SubTask 1", "Description for SubTask 1", false, 1,
+                LocalDateTime.now(), LocalDateTime.now().plusMinutes(30));
+
+        assertThat(subTask.getId()).isNotNull();
+        assertThat(subTask.getTitle()).isEqualTo("SubTask 1");
+        assertThat(subTask.getDescription()).isEqualTo("Description for SubTask 1");
+        assertThat(subTask.getStartTime()).isNotNull();
+        assertThat(subTask.getFinishTime()).isNotNull();
+    }
 }
